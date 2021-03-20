0e9094418b3381ab593cff06d835e10ddce8b4e6
127333 <127333@student.uni.opole.pl>
2021-03-20 18:47:19 +0100

    Zad 01 - Done

---
+++ Javalists/src/List3/Zad01.java
@@ -0,0 +1,17 @@
+package List3;
+
+public class Zad01 {
+
+	public static void main(String[] args) {
+		String NamSur="Imie nazwisko";
+		String [] ns = NamSur.split(" ");
+		
+		for (String x: ns)
+		{
+			System.out.println(x);
+		}
+		
+
+	}
+
+}
