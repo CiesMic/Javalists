c70dcd326490b7b18fa992e8e4f720132f66fd82
127333 <127333@student.uni.opole.pl>
2021-03-20 18:52:56 +0100

    Closed scanner

--- Javalists/src/List3/Zad02.java
+++ Javalists/src/List3/Zad02.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class Zad02 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		Scanner sc = new Scanner(System.in);
 
@@ -14,33 +13,9 @@ public class Zad02 {
 
 		System.out.println("Podaj s³owo (nie)bêd¹ce podzbiorem pierwszego");
 		String shor = sc.next();
-
-		// int k=0;
-
-		// for(int i=0; i<longslowo.length() - shor.length();i++)
-		// {
-		// if (longslowo.charAt(i) == shor.charAt(0))
-		// {
-		// for (int j=1;j<shor.length();j++)
-		// {
-		// if(longslowo.charAt(i+j) == shor.charAt(j))
-		// {
-		// k++;
-		// if (k == shor.length()-1)
-		// {
-		// System.out.println("Jest pozbiorem");
-		// return;
-		// }
-		// }
-		// else
-		// {
-		// k = 0;
-		// break;
-		// }
-		// }
-		// }
-		// }
-
+		
+		sc.close();
+		
 		for (int i = 0; i < longslowo.length(); i++) {
 			if (longslowo.charAt(i) == shor.charAt(0)) {
 				if (shor.equals(longslowo.substring(i, i + shor.length()))) {
@@ -49,6 +24,7 @@ public class Zad02 {
 				}
 			}
 		}
+	
 
 		System.out.println("Nie jest pozbiorem");
 	}
