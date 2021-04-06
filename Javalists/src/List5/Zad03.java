487d4fe230c85e254afa8991d6f59a00250d1635
Michał Cieśla <127333@student.uni.opole.pl>
2021-04-06 19:13:26 +0200

    Created task nr. 3

--- Javalists/src/List5/Zad03.java
+++ Javalists/src/List5/Zad03.java
@@ -1,10 +1,53 @@
 package List5;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Scanner;
+
 public class Zad03 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
 
+		Date data;
+		List<Date> listdate = new ArrayList<Date>();
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+
+		Scanner sc = new Scanner(System.in);
+
+		boolean is_Duplicate;
+		while (true) {
+			try {
+				is_Duplicate = false;
+				System.out.println("Podaj date wg formatu dd.MM.yyyy");
+				data = sdf.parse(sc.next());
+				if (listdate.size() < 4 & listdate.size() > 0) {
+					for (int i = 0; i < listdate.size(); i++) {
+						if (((Date) listdate.get(i)).equals(data)) {
+							is_Duplicate = true;
+						}
+					}
+					if (!is_Duplicate) {
+						listdate.add(data);
+						Collections.sort(listdate);
+						System.out.println(listdate);
+					} else {
+						System.out.println("Duplicated");
+					}
+				} else if (listdate.size() == 0) {
+					listdate.add(data);
+				} else if (listdate.size() > 3) {
+					System.out.println("Is Full");
+					break;
+				}
+			} catch (ParseException e) {
+				System.out.println("Wrong format");
+			}
+		}
 	}
 
 }
